52a9b2f849193ebf714cb588e094bfdb75ffee62
Anderson <andersonsilvacosta@live.com>
2025-09-17 21:41:55 -0300

    update

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/api/pessoa/:id', (req, res) => {
-  //query params
-  //inicializa url depois do ?
-  //loccalhost:3000/api?nome=joao&idade=10&=joinville&status=dead
-
-const nome = req.query.nome
-    
-    res.status(200).send("hello"+ nome);
-});
-
+import express from "express"
+import { exercicio1, exercicio2 } from "./services/exercicio.js"
+ 
+const app = express()
+ 
+// app.get('/exercicio1', (req, res) => {
+//     const num1 = req.query.num1
+//     const num2 = req.query.num2
+//     console.log(req.params)
+ 
+//     res.status(200).send("Hello "+ nome)
+// })
+   
+// localhost:3000/exercicio1?num1=30&num2=45
+app.get('/exercicio1', exercicio1)
+ 
+// localhost:3000/exercicio2/10/2
+app.get('/exercicio2/:valorHoras/:qtdeHoras', exercicio2)
+ 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
-});
-
-app.get('/exercicio1/node/', (req, res) =>{
-    const num1 = parseFloat(req.params.num1)
-    const num2 = parseFloat(req.params.num2)
-    console.log(req.params)
-    const resultado = num1 + num2
-    res.status(200).send(`Resultado ${resultado}`)
+    console.log("Servidor rodando na porta 3000")
 })
